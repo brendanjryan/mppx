e41252fe7b8e0930c4947459ef2e9d90e3134b28
jxom <7336481+jxom@users.noreply.github.com>
2026-02-10 10:03:43 +1100

    chore: parseAccount

--- src/viem/Account.ts
+++ src/viem/Account.ts
@@ -1,4 +1,5 @@
-import { type Address, type Client, isAddressEqual, type Account as viem_Account } from 'viem'
+import type { Address, Client, Account as viem_Account } from 'viem'
+import { parseAccount } from 'viem/accounts'
 
 export type Account = viem_Account
 
@@ -17,21 +18,7 @@ export function getResolver(parameters: getResolver.Parameters = {}) {
       return client.account
     }
 
-    if (typeof account === 'string') {
-      // TODO: remove once JSON-RPC account is supported.
-      if (!client.account)
-        throw new Error(
-          `Address "${account}" was provided but the client does not have an account.`,
-        )
-      // TODO: remove once JSON-RPC account is supported.
-      if (!isAddressEqual(client.account.address, account))
-        throw new Error(
-          `Address "${account}" does not match the client account "${client.account.address}".`,
-        )
-      return client.account
-    }
-
-    return account
+    return parseAccount(account)
   }
 }
 
